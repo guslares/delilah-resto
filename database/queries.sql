97fb0126b6db618f38c76541b272d1728676aaa6
guslares <guslares@gmail.com>
2022-05-24 22:43:40 -0300

    cambios en yaml,valores para usuarios y productos

--- database/queries.sql
+++ database/queries.sql
@@ -38,3 +38,86 @@ CREATE TABLE order_products (
   FOREIGN KEY(product_id) REFERENCES products(product_id)
  
 );
+
+
+INSERT INTO
+  users
+VALUES(
+  NULL,
+  "Pepe Jr",
+  "Grillo",
+  "pepGrillo",
+  "Pepe123",
+  "Calle Falsa 1234",
+  "pepgrillo@gmail.com",
+  "3515054767",
+  1,
+  0
+);
+INSERT INTO
+  users
+VALUES(
+  NULL,
+  "Pepe",
+  "Grillito",
+  "pepGrillito",
+  "Grillito123",
+  "Calle Falsa 147258",
+  "pepegrillito@gmail.com",
+  "351987654",
+  0,
+  0
+);
+INSERT INTO
+  users
+VALUES(
+  NULL,
+  "Homero",
+  "Thomson",
+  "homerThomson",
+  "54321Casa",
+  "Calle 456",
+  "homerThomson@gmail.com",
+  "351987665",
+  0,
+  0
+);
+INSERT INTO
+  users
+VALUES(
+  NULL,
+  "Lalo",
+  "Landa",
+  "laloLanda",
+  "123Casa",
+  "Calle 456",
+  "laloLanda@gmail.com",
+  "3515054760",
+  0,
+  0
+);
+
+INSERT INTO
+products
+VALUES(
+  NULL,
+  "Hamburguesa Simple",
+  "Medallon de carne,queso,lechuga y tomate",
+  500
+)
+INSERT INTO
+products
+VALUES(
+  NULL,
+  "Hamburguesa Especial",
+  "Medallon de carne,queso,lechuga, tomate, huevo y cebolla caramelizada",
+  600
+)
+INSERT INTO
+products
+VALUES(
+  NULL,
+  "Pizza",
+  "Muzarella y aceitunas",
+  650
+)
